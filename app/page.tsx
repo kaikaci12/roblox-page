90578e1b377f1f2c4d93d96ef68bdc5455d7d4d5
kaikaci12 <somewonesomewone@gmail.com>
2024-11-08 23:27:16 +0400

    remove errors

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,3 @@
-import Head from "next/head";
-import NavBar from "./components/NavBar";
-import RobuxBox from "./components/RobuxBox";
 import Middle from "./components/Middle";
 import GetRobux from "./components/GetRobux";
 export default function Home() {
