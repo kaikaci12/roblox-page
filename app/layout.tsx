d4e2f7e932f4f66ad313625300d5633044880b4f
kaikaci12 <somewonesomewone@gmail.com>
2024-11-12 23:15:09 +0400

    add logo and desc

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,12 @@ import NavBar from "./components/NavBar";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "vipbux",
-  description: "",
+  title: "viprbx",
+  description:
+    "Get free currency for the game called roblox , Visit this site to get funds for your next game on roblox.",
+  icons: {
+    icon: [{ rel: "icon", url: "/images/web-logo.png", sizes: "any" }],
+  },
 };
 
 export default function RootLayout({
