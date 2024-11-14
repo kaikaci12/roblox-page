acb332ec8d95709e4191bb39a6d736f743a86295
kaikaci12 <somewonesomewone@gmail.com>
2024-11-14 22:47:33 +0400

    add google adsense

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import Head from "next/head"; // Import Head from next/head
 export const metadata: Metadata = {
   title: "viprbx",
   description:
-    "Get free currency for the game called roblox , Visit this site to get funds for your next game on roblox.",
+    "Get free currency for the game called roblox, Visit this site to get funds for your next game on roblox.",
   icons: {
     icon: [{ rel: "icon", url: "/images/web-logo.png", sizes: "any" }],
   },
@@ -20,13 +20,17 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        {/* Add Google AdSense Script in Head */}
         <Head>
           <script
             async
             src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5356953527878151"
             crossOrigin="anonymous"
           ></script>
+
+          <meta
+            name="google-adsense-account"
+            content="ca-pub-5356953527878151"
+          />
         </Head>
 
         <NavBar />
