7701e2c757cdf12ccc9fcf09bb1578053772ad3e
kaikaci12 <somewonesomewone@gmail.com>
2024-11-27 17:02:01 +0400

    update again

--- app/components/ClickTracker.tsx
+++ app/components/ClickTracker.tsx
@@ -5,13 +5,12 @@ import Offer from "./Offer";
 
 const ClickTracker = ({ children }: { children: React.ReactNode }) => {
   const [showOffer, setShowOffer] = useState(false);
-  const [clicks, setClicks] = useState(0); // Track number of clicks
+  const [clicks, setClicks] = useState(0);
 
   useEffect(() => {
     const handleClick = () => {
       setClicks((prevClicks) => {
         const newClickCount = prevClicks + 1;
-        console.log(clicks);
 
         if (newClickCount % 3 === 0 && !showOffer) {
           setShowOffer(true);
@@ -26,11 +25,11 @@ const ClickTracker = ({ children }: { children: React.ReactNode }) => {
     return () => {
       window.removeEventListener("click", handleClick);
     };
-  }, [showOffer]); // Only update if showOffer changes
+  }, [showOffer, clicks]);
 
   const removeOffer = () => {
     setShowOffer(false);
-    setClicks(0); // Reset click count when the offer is removed
+    setClicks(0);
   };
 
   return (
